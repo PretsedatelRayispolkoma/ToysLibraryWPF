d67b646412b30d48d93a1eb69696abd9446468e8
Diyar Tesfaye <mr.vasya10.10@gmail.com>
2021-06-25 16:22:37 +0300

    added new object book

---
+++ ToysLibrary/Library.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToysLibrary
+{
+    public class Toy
+    {
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+        public int Number { get; set; }
+
+        public Toy(string ttle, string pblshr, int nmbr)
+        {
+            Title = ttle;
+            Publisher = pblshr;
+            Number = nmbr;
+        }
+    }
+
+    public class Book : Toy
+    {
+        public string Author { get; set; }
+        public bool Illustration { get; set;}
+
+        public Book(string ttle, string athr, bool ilstr, string pblshr, int nmbr) : base (ttle, pblshr, nmbr)
+        {
+            Author = athr;
+            Illustration = ilstr;
+        }
+
+    }
+
+    public class Puzzle : Toy
+    {
+        public Puzzle(string ttle, string pblshr, int nmbr) : base(ttle, pblshr, nmbr)
+        { }
+    }
+
+    public class TableToy : Toy
+    {
+        public string Description { get; set; }
+        public int MaxPlayers { get; set; }
+
+        public TableToy(string ttle, string pblshr, string dscrptn, int nmbr, int mxplyers) : base(ttle, pblshr, nmbr)
+        {
+            Description = dscrptn;
+            MaxPlayers = mxplyers;
+        }
+    }
+}
--- ToysLibraryWPF/MainWindow.xaml.cs
+++ ToysLibraryWPF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ToysLibrary;
 
 namespace ToysLibraryWPF
 {
@@ -20,9 +21,26 @@ namespace ToysLibraryWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        Book book;
+        string t;
+        string a;
+        bool i;
+        string p;
+        int y;
+
         public MainWindow()
         {
+            t = TB_title.Text;
+            a = TB_author.Text;
+            i = Convert.ToBoolean(Convert.ToInt32(TB_author.Text));
+            p = TB_publisher.Text;
+            y = int.Parse(TB_year.Text);
             InitializeComponent();
+            book = new Book(t, a, i, p, y);
+        }
+
+        private void ToysLibrary_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        { 
         }
     }
 }
